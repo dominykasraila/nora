0fa1385a49ad66574b47c8a92791b61d15812391
dominykasraila <dominykas.raila@gmail.com>
2024-12-14 20:04:00 +0200

    Changes for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,15 +15,20 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Support env variables from .env file if defined
+import os
+from dotenv import load_dotenv
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nnbwq!68s7k_0#s2e*5h)rj+=-24sb@*z^(m=^_1h&91#1ojx0'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-nnbwq!68s7k_0#s2e*5h)rj+=-24sb@*z^(m=^_1h&91#1ojx0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
     'elated2337.eu.pythonanywhere.com',
@@ -119,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
